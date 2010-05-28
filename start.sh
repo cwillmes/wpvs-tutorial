326ca67fde06f799e6acb8ece777f992c71e2aa8
Markus Schneider <schneider@schneider-desktop.(none)>
2010-05-28 12:25:33 +0200

    Added the data directory to .gitignore

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+# only adapt the following two lines
 export LD_LIBRARY_PATH=/home/schneider/dev/java-libs/jogl-1.1.1a-linux-amd64/lib
 export D3_TOOLBOX=~/workspace/deegree-tools/target/appassembler/bin/d3toolbox
+
 $D3_TOOLBOX InteractiveWPVS -wpvsconfig src/main/webapp/WEB-INF/conf
