0113fc7661fb26977034ce622e48626d75feb6f9
Markus Schneider <markus@beefcafe.de>
2010-06-07 17:39:03 +0200

    Added buildings.

--- prepare-dem.sh
+++ prepare-dem.sh
@@ -1,6 +1,6 @@
 # only adapt the following two lines
-export D3_TOOLBOX=~/Programmieren/Java/workspace/deegree-tools/target/appassembler/bin/d3toolbox
-export DEM_SRC=~/Programmieren/Data/geodaten/christian_wilmes/Campus3D/dem/5m_dem/campusDEM.tif
+export D3_TOOLBOX=~/workspace/deegree-tools/target/appassembler/bin/d3toolbox
+export DEM_SRC=~/geodaten/christian_wilmes/Campus3D/dem/5m_dem/campusDEM.tif
 
 # create Multiresolution DEM in 'src/main/webapp/WEB-INF/data/batchedmt'
 export TARGET_DIR=src/main/webapp/WEB-INF/data/batchedmt
--- prepare-renderables.sh
+++ prepare-renderables.sh
@@ -1,6 +1,6 @@
-# only adapt the following three lines
-D3_TOOLBOX=~/Programmieren/Java/workspace/deegree-tools/target/appassembler/bin/d3toolbox
-BUILDING_DIR=~/Programmieren/Data/geodaten/christian_wilmes/Campus3D/Buildings/d3buildings
+# only adapt the following two lines
+D3_TOOLBOX=~/workspace/deegree-tools/target/appassembler/bin/d3toolbox
+BUILDING_DIR=~/geodaten/christian_wilmes/Campus3D/Buildings/d3buildings
 
 TARGET_DIR=src/main/webapp/WEB-INF/data/coverage/renderable/buildings
 rm -Rf $TARGET_DIR
@@ -9,5 +9,5 @@ INPUT_FILES="$BUILDING_DIR/*.gml"
 for f in $INPUT_FILES
 do
   echo "Processing $f..."
-  $D3_TOOLBOX DataManager -a import -f $f -host org.deegree.services.wpvs.io.file.FileBackend -fbd $TARGET_DIR -t building -ql 2 -mtd 1024x1024 -tt "-2569766.263000,-5655753.758000" -v
+  $D3_TOOLBOX DataManager -a import -f $f -host org.deegree.services.wpvs.io.file.FileBackend -fbd $TARGET_DIR -t building -ql 2 -mtd 1024x1024 -tt "-2560250.0,-5638990.0" -v
 done
