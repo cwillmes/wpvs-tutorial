6c3be8ceb76a90056e72cb8799842afd3b29b819
Christian Willmes <christian.willmes@gmail.com>
2010-11-23 21:02:48 +0100

    added the changed files

--- prepare-dem.sh
+++ prepare-dem.sh
@@ -1,9 +1,9 @@
 # only adapt the following two lines
-export D3_TOOLBOX=~/workspace/deegree-tools/target/appassembler/bin/d3toolbox
-export DEM_SRC=~/geodaten/christian_wilmes/Campus3D/dem/5m_dem/campusDEM.tif
+export D3_TOOLBOX=~/dev/d3/deegree-tools/target/appassembler/bin/d3toolbox
+export DEM_SRC=~/dev/wpvs-tutorial/Data_23030/DEM/barcelona_23030_dem.tif
 
 # create Multiresolution DEM in 'src/main/webapp/WEB-INF/data/batchedmt'
 export TARGET_DIR=src/main/webapp/WEB-INF/data/batchedmt
 rm -Rf $TARGET_DIR 
 mkdir -p $TARGET_DIR
-$D3_TOOLBOX DEMDatasetGenerator -rl $DEM_SRC -it tif -s_srs EPSG:31466 -o $TARGET_DIR -ol 10 -or 64 -mh 150 -v
+$D3_TOOLBOX DEMDatasetGenerator -rl $DEM_SRC -it tif -s_srs EPSG:23030 -o $TARGET_DIR -ol 10 -or 64 -mh 150 -v
--- prepare-renderables.sh
+++ prepare-renderables.sh
@@ -1,6 +1,6 @@
 # only adapt the following two lines
-D3_TOOLBOX=~/workspace/deegree-tools/target/appassembler/bin/d3toolbox
-BUILDING_DIR=~/geodaten/christian_wilmes/Campus3D/Buildings/d3buildings
+D3_TOOLBOX=~/dev/d3/deegree-tools/target/appassembler/bin/d3toolbox
+BUILDING_DIR=~/dev/wpvs-tutorial/Data_23030/Buildings
 
 TARGET_DIR=src/main/webapp/WEB-INF/data/renderable/buildings
 rm -Rf $TARGET_DIR
@@ -9,5 +9,5 @@ INPUT_FILES="$BUILDING_DIR/*.gml"
 for f in $INPUT_FILES
 do
   echo "Processing $f..."
-  $D3_TOOLBOX DataManager -a import -f $f -host org.deegree.services.wpvs.io.file.FileBackend -fbd $TARGET_DIR -t building -ql 2 -mtd 1024x1024 -tt "-2560250.0,-5638990.0" -v
+  $D3_TOOLBOX DataManager -a import -f $f -host org.deegree.services.wpvs.io.file.FileBackend -fbd $TARGET_DIR -t building -ql 2 -mtd 1024x1024 -tt "-925892.424,-4589021.314" -v
 done
